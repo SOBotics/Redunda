ff6e171820170623e1b555ba66c7c0563cc4f17a
Jonathan Keller <jkeller234@icloud.com>
2017-03-07 19:58:58 -0800

    fix whitespace, add path helpers

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -2,36 +2,36 @@ class BotsController < ApplicationController
   before_action :set_bot, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :check_bot_ownership, only: [:edit, :update, :destroy]
-  
+
   # GET /bots
   # GET /bots.json
   def index
     @bots = Bot.all
   end
-  
+
   # GET /bots/1
   # GET /bots/1.json
   def show
   end
-  
+
   # GET /bots/new
   def new
     @bot = Bot.new
   end
-  
+
   # GET /bots/1/edit
   def edit
   end
-  
+
   # POST /bots
   # POST /bots.json
   def create
     @bot = Bot.new(bot_params)
-    
+
     respond_to do |format|
       if @bot.save
         current_user.add_role :owner, @bot
-        
+
         format.html { redirect_to bots_path, notice: 'Bot was successfully created.' }
         format.json { render :show, status: :created, location: @bot }
         else
@@ -40,7 +40,7 @@ class BotsController < ApplicationController
       end
     end
   end
-  
+
   # PATCH/PUT /bots/1
   # PATCH/PUT /bots/1.json
   def update
@@ -54,7 +54,7 @@ class BotsController < ApplicationController
       end
     end
   end
-  
+
   # DELETE /bots/1
   # DELETE /bots/1.json
   def destroy
@@ -64,14 +64,14 @@ class BotsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
-  
+
+
   # POST /bots/1/collaborators
   # POST /bots/1/collaborators.json
   def add_collaborator
     bot = Bot.find(params[:bot])
     collaborator = User.find(params[:collaborator])
-    
+
     if collaborator.has_role?(:collaborator, bot)
       respond_to do |format|
         format.html { redirect_to edit_bot_path(bot), notice: 'That user is already a collaborator.' }
@@ -79,22 +79,22 @@ class BotsController < ApplicationController
       end
       return
     end
-    
-    
+
+
     collaborator.add_role :collaborator, bot
-    
+
     respond_to do |format|
       format.html { redirect_to edit_bot_path(bot), notice: 'Collaborator was successfully added.' }
       format.json { head :no_content }
     end
   end
-  
-  
-  
+
+
+
   def remove_collaborator
     bot = Bot.find(params[:bot])
     collaborator = User.find(params[:collaborator])
-    
+
     unless collaborator.has_role?(:collaborator, bot)
       respond_to do |format|
         format.html { redirect_to edit_bot_path(bot), notice: 'That user is not a collaborator.' }
@@ -102,27 +102,27 @@ class BotsController < ApplicationController
       end
       return
     end
-    
-    
+
+
     collaborator.remove_role :collaborator, bot
-    
+
     respond_to do |format|
       format.html { redirect_to edit_bot_path(bot), notice: 'Collaborator was successfully removed.' }
       format.json { head :no_content }
     end
   end
-  
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_bot
     @bot = Bot.find(params[:id])
   end
-  
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def bot_params
     params.require(:bot).permit(:name)
   end
-  
+
   def check_bot_ownership
     render :status => :forbidden, :plain => "You don't own this bot" and return unless current_user.has_role? :owner, @bot
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :bots do
     resources :bot_instances
   end
-  post 'bots/:bot/collaborators', to: 'bots#add_collaborator'
+  post 'bots/:bot/collaborators', to: 'bots#add_collaborator', as: :add_collaborator
   
-  delete 'bots/:bot/collaborators/:collaborator', to: 'bots#remove_collaborator'
+  delete 'bots/:bot/collaborators/:collaborator', to: 'bots#remove_collaborator', as: :remove_collaborator
 
   scope "authentication" do
     get 'login_redirect_target', to: 'authentication#login_redirect_target'
