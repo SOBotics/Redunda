1792fff2d315b69ef7169695bc636bda7308c2df
Jonathan Keller <jkeller234@icloud.com>
2017-03-07 19:38:51 -0800

    Fix indentation, redundant error checking, and hardcoded URLS

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -2,45 +2,45 @@ class BotsController < ApplicationController
   before_action :set_bot, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :check_bot_ownership, only: [:edit, :update, :destroy]
-
+  
   # GET /bots
   # GET /bots.json
   def index
     @bots = Bot.all
   end
-
+  
   # GET /bots/1
   # GET /bots/1.json
   def show
   end
-
+  
   # GET /bots/new
   def new
     @bot = Bot.new
   end
-
+  
   # GET /bots/1/edit
   def edit
   end
-
+  
   # POST /bots
   # POST /bots.json
   def create
     @bot = Bot.new(bot_params)
-
+    
     respond_to do |format|
       if @bot.save
         current_user.add_role :owner, @bot
-
+        
         format.html { redirect_to bots_path, notice: 'Bot was successfully created.' }
         format.json { render :show, status: :created, location: @bot }
-      else
+        else
         format.html { render :new }
         format.json { render json: @bot.errors, status: :unprocessable_entity }
       end
     end
   end
-
+  
   # PATCH/PUT /bots/1
   # PATCH/PUT /bots/1.json
   def update
@@ -48,13 +48,13 @@ class BotsController < ApplicationController
       if @bot.update(bot_params)
         format.html { redirect_to @bot, notice: 'Bot was successfully updated.' }
         format.json { render :show, status: :ok, location: @bot }
-      else
+        else
         format.html { render :edit }
         format.json { render json: @bot.errors, status: :unprocessable_entity }
       end
     end
   end
-
+  
   # DELETE /bots/1
   # DELETE /bots/1.json
   def destroy
@@ -69,60 +69,30 @@ class BotsController < ApplicationController
   # POST /bots/1/collaborators
   # POST /bots/1/collaborators.json
   def add_collaborator
-      unless Bot.exists?(id: params[:bot])
-        respond_to do |format|
-          format.html { redirect_to index_path, notice: 'That bot ID does not exist.' }
-          format.json { render json: 'That bot ID does not exist.', status: :unprocessable_entity }
-        end
-        return
-      end
-      bot = Bot.find(params[:bot])
-      
-      unless User.exists?(id: params[:collaborator])
-        respond_to do |format|
-          format.html { redirect_to edit_bot_path(bot), notice: 'That user ID does not exist.' }
-          format.json { render json: 'That user ID does not exist.', status: :unprocessable_entity }
-        end
-        return
-      end
-      collaborator = User.find(params[:collaborator])
-      
-      if collaborator.has_role?(:collaborator, bot)
-        respond_to do |format|
-          format.html { redirect_to edit_bot_path(bot), notice: 'That user is already a collaborator.' }
-          format.json { render json: 'That user is already a collaborator.', status: :unprocessable_entity }
-        end
-        return
-      end
-      
-      
-      collaborator.add_role :collaborator, bot
-      
+    bot = Bot.find(params[:bot])
+    collaborator = User.find(params[:collaborator])
+    
+    if collaborator.has_role?(:collaborator, bot)
       respond_to do |format|
-        format.html { redirect_to edit_bot_path(bot), notice: 'Collaborator was successfully added.' }
-        format.json { head :no_content }
+        format.html { redirect_to edit_bot_path(bot), notice: 'That user is already a collaborator.' }
+        format.json { render json: 'That user is already a collaborator.', status: :unprocessable_entity }
       end
+      return
+    end
+    
+    
+    collaborator.add_role :collaborator, bot
+    
+    respond_to do |format|
+      format.html { redirect_to edit_bot_path(bot), notice: 'Collaborator was successfully added.' }
+      format.json { head :no_content }
+    end
   end
   
   
   
   def remove_collaborator
-    unless Bot.exists?(id: params[:bot])
-      respond_to do |format|
-        format.html { redirect_to index_path, notice: 'That bot ID does not exist.' }
-        format.json { render json: 'That bot ID does not exist.', status: :unprocessable_entity }
-      end
-      return
-    end
     bot = Bot.find(params[:bot])
-    
-    unless User.exists?(id: params[:collaborator])
-      respond_to do |format|
-        format.html { redirect_to edit_bot_path(bot), notice: 'That user ID does not exist.' }
-        format.json { render json: 'That user ID does not exist.', status: :unprocessable_entity }
-      end
-      return
-    end
     collaborator = User.find(params[:collaborator])
     
     unless collaborator.has_role?(:collaborator, bot)
@@ -141,19 +111,19 @@ class BotsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bot
-      @bot = Bot.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def bot_params
-      params.require(:bot).permit(:name)
-    end
-
-    def check_bot_ownership
-      render :status => :forbidden, :plain => "You don't own this bot" and return unless current_user.has_role? :owner, @bot
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bot
+    @bot = Bot.find(params[:id])
+  end
+  
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def bot_params
+    params.require(:bot).permit(:name)
+  end
+  
+  def check_bot_ownership
+    render :status => :forbidden, :plain => "You don't own this bot" and return unless current_user.has_role? :owner, @bot
+  end
 end
