8d1dd0ecc3fe07884245ac4ae3e7efb19039de28
Jonathan Keller <jkeller234@icloud.com>
2017-03-09 16:13:48 -0800

    sort users alphabetically in the picker

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -23,6 +23,7 @@ class BotsController < ApplicationController
   def edit
     @eligible_collaborators = User.where.not(:id => User.with_role(:owner, @bot).pluck(:id))
                                   .where.not(:id => User.with_role(:collaborator, @bot).pluck(:id))
+                                  .order(:username)
                                   .map{ |u| [u.username, u.id] }
   end
 
