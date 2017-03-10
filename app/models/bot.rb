e42c2ce06339bed21fb24f788e5a931d17a53722
Jonathan Keller <jkeller234@icloud.com>
2017-03-09 16:52:37 -0800

    finish resolving merge conflict

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -6,6 +6,7 @@ class Bot < ApplicationRecord
   def eligible_collaborators
     User.where.not(:id => User.with_role(:owner, self).pluck(:id))
         .where.not(:id => User.with_role(:collaborator, self).pluck(:id))
+        .order(:username)
         .map{ |u| [u.username, u.id] }
   end
 end
