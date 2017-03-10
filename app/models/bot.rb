dd6ddc88cff3ef79a1233b6f5b61e00f9eefcd7e
Undo1 <parker@erwaysoftware.com>
2017-03-09 17:25:06 -0700

    AJAXify collaborator addition and removal

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -2,4 +2,10 @@ class Bot < ApplicationRecord
   resourcify
 
   has_many :bot_instances
+
+  def eligible_collaborators
+    User.where.not(:id => User.with_role(:owner, self).pluck(:id))
+        .where.not(:id => User.with_role(:collaborator, self).pluck(:id))
+        .map{ |u| [u.username, u.id] }
+  end
 end
