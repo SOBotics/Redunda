b711d7787c54b0a4a640e6c38e1954a954878fa5
Undo1 <parker@erwaysoftware.com>
2018-06-25 22:04:36 -0600

    Use key on username requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable
 
+  def after_database_authentication
+    Thread.new do
+      begin
+        update(username: get_username(nil, stack_exchange_account_id))
+      rescue
+      end
+    end
+  end
+
   def get_username(readonly_api_token, network_user_id=nil)
     begin
       config = AppConfig["stack_exchange"]
 
       resp = if network_user_id.present?
-        resp = JSON.parse(Net::HTTP.get_response(URI.parse("https://api.stackexchange.com/2.2/users/#{network_user_id}/associated?pagesize=1&filter=!-rYuN4gZ")).body)
+        resp = JSON.parse(Net::HTTP.get_response(URI.parse("https://api.stackexchange.com/2.2/users/#{network_user_id}/associated?pagesize=1&filter=!-rYuN4gZ&key=#{config["key"]}")).body)
         first_site = URI.parse(resp["items"][0]["site_url"]).host
         user_id = resp["items"][0]["user_id"]
 
-        JSON.parse(Net::HTTP.get_response(URI.parse("https://api.stackexchange.com/2.2/users/#{user_id}?site=#{first_site}&filter=!-.wwQ56Mfo3J")).body)
+        JSON.parse(Net::HTTP.get_response(URI.parse("https://api.stackexchange.com/2.2/users/#{user_id}?site=#{first_site}&filter=!-.wwQ56Mfo3J&key=#{config["key"]}")).body)
       else
         auth_string = "key=#{config["key"]}&access_token=#{readonly_api_token || api_token}"
         resp = JSON.parse(Net::HTTP.get_response(URI.parse("https://api.stackexchange.com/2.2/me/associated?pagesize=1&filter=!ms3d6aRI6N&#{auth_string}")).body)
